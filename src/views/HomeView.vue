3457f0b6d7064bfdaf6c11a5c12a9c3659228bf5
Salman Patnee <salmanpatni92@gmail.com>
2022-10-07 00:50:04 +0500

    Add single thread page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,9 +6,10 @@ const threads = ref([]);
 const posts = ref([]);
 const users = ref([]);
 
-const getPostById = id => posts.value.find(post => post.id === id);
-const getUserById = id => users.value.find(user => user.id === id);
-const getImageSrc = id => `https://i.pravatar.cc/500?img=${getUserById(getPostById(id).userId).id}`;
+const getPostById = (id) => posts.value.find((post) => post.id === id);
+const getUserById = (id) => users.value.find((user) => user.id === id);
+const getImageSrc = (id) =>
+  `https://i.pravatar.cc/500?img=${getUserById(getPostById(id).userId).id}`;
 
 onMounted(() => {
   threads.value = data.threads;
@@ -18,30 +19,31 @@ onMounted(() => {
 </script>
 
 <template>
-  <main>
-    <div class="container">
-
-      <div v-for="thread in threads" :key="thread.id">
-        <h1>{{thread.title}}</h1>
-        <div class="card mb-3" v-for="postId in thread.posts" :key="postId">
-          <div class="row g-0">
-            <div class="col-md-4">
-              <img :src="getImageSrc(postId)"
-                class="img-fluid rounded-start" :alt="getUserById(getPostById(postId).userId).name">
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <p class="card-text">{{getPostById(postId).text}}</p>
-                <p class="card-text">
-                  <b>By</b>&nbsp;<small class="text-muted">{{getUserById(getPostById(postId).userId).name}}</small>
-                  &nbsp; <b>On</b>&nbsp;{{getPostById(postId).publishedAt}}
-                </p>
-              </div>
+  <div class="container">
+    <div v-for="thread in threads" :key="thread.id">
+      <h1>{{ thread.title }}</h1>
+      <div class="card mb-3" v-for="postId in thread.posts" :key="postId">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img
+              :src="getImageSrc(postId)"
+              class="img-fluid rounded-start"
+              :alt="getUserById(getPostById(postId).userId).name"
+            />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <p class="card-text">{{ getPostById(postId).text }}</p>
+              <p class="card-text">
+                <b>By</b>&nbsp;<small class="text-muted">{{
+                  getUserById(getPostById(postId).userId).name
+                }}</small>
+                &nbsp; <b>On</b>&nbsp;{{ getPostById(postId).publishedAt }}
+              </p>
             </div>
           </div>
         </div>
       </div>
     </div>
-
-  </main>
+  </div>
 </template>
---
+++ src/views/ThreadShow.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+import data from "@/data.json";
+
+const props = defineProps({
+  id: {
+    type: Number,
+    required: true,
+  },
+});
+
+const threads = ref([]);
+const thread = ref([]);
+const posts = ref([]);
+const users = ref([]);
+
+const getPostById = (id) => posts.value.find((post) => post.id === id);
+const getUserById = (id) => users.value.find((user) => user.id === id);
+const getImageSrc = (id) =>
+  `https://i.pravatar.cc/500?img=${getUserById(getPostById(id).userId).id}`;
+
+onMounted(() => {
+  threads.value = data.threads;
+  posts.value = data.posts;
+  users.value = data.users;
+  thread.value = threads.value.find((thread) => thread.id === props.id);
+});
+</script>
+
+<template>
+  <div class="container">
+    <h1>{{ thread.title }}</h1>
+    <div class="card mb-3" v-for="postId in thread.posts" :key="postId">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img
+            :src="getImageSrc(postId)"
+            class="img-fluid rounded-start"
+            :alt="getUserById(getPostById(postId).userId).name"
+          />
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <p class="card-text">{{ getPostById(postId).text }}</p>
+            <p class="card-text">
+              <b>By</b>&nbsp;<small class="text-muted">{{
+                getUserById(getPostById(postId).userId).name
+              }}</small>
+              &nbsp; <b>On</b>&nbsp;{{ getPostById(postId).publishedAt }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
