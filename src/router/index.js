42e6cb7c5dc0fc327e4ed1cf91f21a440c903d89
Salman Patnee <salmanpatni92@gmail.com>
2022-10-08 00:34:51 +0500

    Handle not existing resource.

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import ThreadShow from "@/views/ThreadShow.vue";
 import NotFound from "@/views/NotFound.vue";
+import data from "@/data.json";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,18 @@ const router = createRouter({
       name: "threads.show",
       component: ThreadShow,
       props: true,
+      beforeEnter(to, from, next) {
+        const threadExists = data.threads.find(
+          (thread) => thread.id === to.params.id
+        );
+        if (threadExists) return next();
+        else {
+          next({
+            name: "404",
+            params: { pathMatch: to.path.substring(1).split("/") },
+          });
+        }
+      },
     },
     {
       path: "/:pathMatch(.*)*",
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,9 +1,9 @@
-<script setup>
-
-</script>
+<script setup></script>
 <template>
-    <div class="d-flex justify-content-cente align-items-center text-center">
-
-        <h1>404</h1>
+  <div class="d-flex justify-content-cente align-items-center text-center">
+    <div class="w-100">
+      <h1>404</h1>
+      <p>Sorry could not found what you are looking for.</p>
     </div>
+  </div>
 </template>
